40f5d244e4e31977a27cbf37be8d4e1e20b5812b
Fabio Kusaba <fabinhokb44@gmail.com>
2025-06-02 20:39:27 -0300

    feat: add authentication routes to API

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,23 +12,30 @@ func (app *application) routes() http.Handler {
 	v1 := g.Group("/api/v1")
 	{
 		// Events
-		v1.POST("/events", app.createEvent)
 		v1.GET("/events", app.getAllEvents)
 		v1.GET("/events/:id", app.getEvent)
-		v1.PUT("/events/:id", app.updateEvent)
-		v1.DELETE("/events/:id", app.deleteEvent)
 
 		// Register User
 		v1.POST("/auth/register", app.registerUser)
 
 		// Attendees
-		v1.POST("/events/:id/attendees/:userId", app.addAttendeeToEvent)
 		v1.GET("/events/:id/attendees", app.getAttendeesForEvent)
-		v1.DELETE("/events/:id/attendees/:userId", app.deleteAttendeeFromEvent)
 		v1.GET("/attendees/:id/events", app.getEventsByAttendee)
 
 		// Login
 		v1.POST("/auth/login", app.login)
+	}
+
+	// Authentication
+	authGroup := v1.Group("/")
+	authGroup.Use(app.AuthMiddleware())
+	{
+		authGroup.POST("/events", app.createEvent)
+		authGroup.PUT("/events/:id", app.updateEvent)
+		authGroup.DELETE("/events/:id", app.deleteEvent)
+
+		authGroup.POST("/events/:id/attendees/:userId", app.addAttendeeToEvent)
+		authGroup.DELETE("/events/:id/attendees/:userId", app.deleteAttendeeFromEvent)
 	}
 
 	return g
